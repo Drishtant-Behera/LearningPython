843c0abb387486fbf46c06959b2414d91875fc55
Drishtant-Behera <108445843+Drishtant-Behera@users.noreply.github.com>
2022-12-19 21:27:07 -0500

    added multiplication and division
    added the other two basic operaters doing python is just like doing maths.

--- Operaters/Using_Operators.py
+++ Operaters/Using_Operators.py
@@ -1,1 +1,1 @@
-a = 20b = 5addition = a + bsubtraction = a - bprint("adding 5 and 6 =", addition)
+a = 20b = 5addition = a + bsubtraction = a - bmultiplication = a * bdivision = a / bprint("adding", a, "and", b, "=", addition)print("subtracting", a, "and", b, "=", subtraction)print("multiplying", a, "and", b, "=", multiplication)print("dividing", a, "and", b, "=", division)
