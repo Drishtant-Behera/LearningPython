eaa5c31491a603249b87d519f2cce8cae7edb8f0
Drishtant-Behera <108445843+Drishtant-Behera@users.noreply.github.com>
2022-12-20 22:08:30 -0500

    learned how to use define comand
    Define is a handy command to use especially when your dealing with lots of code you can group the code and when your using the same code again and again then define and call the definition as it will save lots of time

---
+++ Basic Syntax/define.py
@@ -0,0 +1,1 @@
+from math import sqrtdef Power_Calculator(x, y):    answer = x ** y    print(answer)def Square_Root(x):    answer1 = sqrt(x)    print(answer1)Power_Calculator(12, 3)Square_Root(100)
